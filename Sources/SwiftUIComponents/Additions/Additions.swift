68e784622890e1c2375e4421c0bf413fbcad0987
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2025-03-07 11:28:26 +0100

    added move top/bottom transition with block

--- Sources/SwiftUIComponents/Additions/Additions.swift
+++ Sources/SwiftUIComponents/Additions/Additions.swift
@@ -23,20 +23,41 @@ public extension AnyTransition {
 struct MovePositionModifier: ViewModifier {
     let presented: Bool
     let inverse: Bool
+    let multiplier: CGFloat
     let edge: ()->Edge
     
+    var xOffset: CGFloat {
+        let edge = edge()
+        
+        if edge == .leading || edge == .trailing {
+            return presented ? 0 : (UIScreen.main.bounds.width * multiplier * (edge == .leading ? 1 : -1) * (inverse ? -1 : 1))
+        } else {
+            return 0
+        }
+    }
+    
+    var yOffset: CGFloat {
+        let edge = edge()
+        
+        if edge == .top || edge == .bottom {
+            return presented ? 0 : (UIScreen.main.bounds.height * multiplier * (edge == .top ? 1 : -1) * (inverse ? -1 : 1))
+        } else {
+            return 0
+        }
+    }
+    
     func body(content: Content) -> some View {
-        content.offset(x: presented ? 0 : (UIScreen.main.bounds.width * (edge() == .leading ? 1 : -1) * (inverse ? -1 : 1)))
+        content.offset(x: xOffset, y: yOffset)
     }
 }
 
 public extension AnyTransition {
     
-    static func move(edge: @escaping ()->Edge) -> AnyTransition {
-        .asymmetric(insertion: .modifier(active: MovePositionModifier(presented: false, inverse: false, edge: edge),
-                                         identity: MovePositionModifier(presented: true, inverse: false, edge: edge)),
-                    removal: .modifier(active: MovePositionModifier(presented: false, inverse: true, edge: edge),
-                                       identity: MovePositionModifier(presented: true, inverse: true, edge: edge)))
+    static func move(multiplier: CGFloat = 1, edge: @escaping ()->Edge) -> AnyTransition {
+        .asymmetric(insertion: .modifier(active: MovePositionModifier(presented: false, inverse: false, multiplier: multiplier, edge: edge),
+                                         identity: MovePositionModifier(presented: true, inverse: false, multiplier: multiplier, edge: edge)),
+                    removal: .modifier(active: MovePositionModifier(presented: false, inverse: true, multiplier: multiplier, edge: edge),
+                                       identity: MovePositionModifier(presented: true, inverse: true, multiplier: multiplier, edge: edge)))
         
         
     }
